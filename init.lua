0fcd73465c6888e791a3f38e19e68d90da38f068
Liam Arzola <liam0215@gmail.com>
2026-07-29 10:46:13 -0700

    update treesitter / telescope init

--- init.lua
+++ init.lua
@@ -306,7 +306,7 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    tag = 'v0.2.1',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -603,16 +603,6 @@ require('lazy').setup({
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
-      require('mason').setup {
-        install_root_dir = '/data/liam/.local/share/nvim/mason',
-      }
-
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
@@ -889,36 +879,78 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = true,
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+    config = function()
+      local treesitter = require 'nvim-treesitter'
 
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      treesitter.setup {}
+
+      local parsers = {
+        'bash',
+        'c',
+        'cpp',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'vim',
+        'vimdoc',
+        'go',
+        'gomod',
+        'python',
+        'query',
+      }
+
+      -- No-op for parsers that are already installed.
+      treesitter.install(parsers)
+
+      local group = vim.api.nvim_create_augroup('user_treesitter', { clear = true })
+
+      -- Highlighting is now enabled through Neovim itself.
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        callback = function(args)
+          local filetype = vim.bo[args.buf].filetype
+          local language = vim.treesitter.language.get_lang(filetype) or filetype
+
+          if vim.tbl_contains(parsers, language) then
+            pcall(vim.treesitter.start, args.buf, language)
+          end
+        end,
+      })
+
+      -- Tree-sitter indentation remains experimental. Enable it only for
+      -- filetypes where it is generally useful.
+      local indent_languages = {
+        bash = true,
+        c = true,
+        cpp = true,
+        html = true,
+        lua = true,
+        vim = true,
+        go = true,
+        gomod = true,
+        python = true,
+      }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        callback = function(args)
+          local filetype = vim.bo[args.buf].filetype
+          local language = vim.treesitter.language.get_lang(filetype) or filetype
+
+          if indent_languages[language] then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
     end,
   },
 
